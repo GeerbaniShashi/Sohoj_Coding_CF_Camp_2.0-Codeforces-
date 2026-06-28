19710dc6779ffe2adda7d705a32a9eb453cd06ab
Geerbani Pal Shashi <120784788+GeerbaniShashi@users.noreply.github.com>
2026-06-28 21:19:54 +0600

    Add files via upload

---
+++ 1883B_Chemistry.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n, k;
+        cin>>n>>k;
+        map<char, ll>mp;
+        map<char, ll>::iterator it;
+        string s;
+        cin>>s;
+        for(ll i=0; i<n; i++)
+        {
+            mp[s[i]]++;
+        }
+        ll diff = 0;
+        for(it = mp.begin(); it != mp.end(); it++)
+        {
+            if((*it).second % 2 == 1) diff++;
+        }
+        if(diff <= k+1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
